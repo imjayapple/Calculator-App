5232d2c0255956ddfb9394585f4de22cb45da71e
imjayapple <imjayapple@gmail.com>
2024-06-08 12:51:33 -0700

    JS, calculate what's on the display, and use a try/catch block to assist with error-handling.

--- calculator.js
+++ calculator.js
@@ -13,5 +13,10 @@ function clearDisplay(){
 }
 
 function calculate(){
-
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
 }
